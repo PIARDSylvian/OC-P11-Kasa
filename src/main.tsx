13b89fef4b617f7d11985bca95d631e90131a6a7
PIARDSylvian <piard.sylvian@gmail.com>
2023-03-28 19:54:50 +0200

    routing and pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import Router from '../src/components/Router'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Router />
+  </React.StrictMode>
 )
