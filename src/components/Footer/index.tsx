e556834f3d942b7a60ed4be8a2fcc2477e89015c
PIARDSylvian <piard.sylvian@gmail.com>
2023-03-29 13:02:06 +0200

    footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,13 @@
-import './style.scss'
+import logo from '../../assets/kasa-logo.png'
+import style from './style.module.scss'
 
 function Footer() {
-  return <footer>Footer</footer>
+  return (
+    <footer className={style.footer}>
+      <img src={logo} className={style.logo} alt="kasa logo" />
+      <p>© 2020 Kasa. All rights reserved</p>
+    </footer>
+  )
 }
 
 export default Footer
