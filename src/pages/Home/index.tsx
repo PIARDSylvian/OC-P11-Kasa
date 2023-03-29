063936d393901312eecac0bf0aa23d0e68ecef9f
PIARDSylvian <piard.sylvian@gmail.com>
2023-03-30 00:03:00 +0200

    home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,23 @@
 import style from './style.module.scss'
+import Banner from '../../components/Banner'
+import Card from '../../components/Card'
+import data from '../../../backend/data.json'
+import cover from '../../assets/home-header.jpg'
 
 function Home() {
   return (
     <section>
-      <header className={style.header}>
+      <Banner height={223} mobileHeight={111} cover={cover}>
         <h1>Chez vous, partout et ailleurs</h1>
-      </header>
+      </Banner>
       <div className={style.logements_wrapper}>
-        <p>Home</p>
+        {data.map((logement) => (
+          <Card
+            key={`logement-${logement.id}`}
+            title={logement.title}
+            image={logement.cover}
+          />
+        ))}
       </div>
     </section>
   )
