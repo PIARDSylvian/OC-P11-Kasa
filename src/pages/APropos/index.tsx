398740387ebaea693983b96dab03b32b034cb165
PIARDSylvian <piard.sylvian@gmail.com>
2023-03-30 15:14:15 +0200

    a-propos collapse

--- src/pages/APropos/index.tsx
+++ src/pages/APropos/index.tsx
@@ -1,5 +1,43 @@
+import Collapse from '../../components/Collapse'
+import style from './style.module.scss'
+
 function APropos() {
-  return <p>APropos</p>
+  return (
+    <>
+      <div className={style.collapse_wrapper}>
+        <Collapse
+          collapseTitle="Fiabilité"
+          collapseList={[
+            'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.',
+          ]}
+        />
+      </div>
+      <div className={style.collapse_wrapper}>
+        <Collapse
+          collapseTitle="Respect"
+          collapseList={[
+            'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
+          ]}
+        />
+      </div>
+      <div className={style.collapse_wrapper}>
+        <Collapse
+          collapseTitle="Service"
+          collapseList={[
+            "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+          ]}
+        />
+      </div>
+      <div className={style.collapse_wrapper}>
+        <Collapse
+          collapseTitle="Sécurité"
+          collapseList={[
+            "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+          ]}
+        />
+      </div>
+    </>
+  )
 }
 
 export default APropos
